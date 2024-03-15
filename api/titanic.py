36beecbdb3610371e8df138ee652761df2872194
Imaad08 <imaadmuzaffer@gmail.com>
2024-03-14 19:27:55 -0700

    e

--- api/titanic.py
+++ api/titanic.py
@@ -12,7 +12,6 @@ api = Api(titanic_api)
 
 class TitanicAPI(Resource):
     def __init__(self):
-        # Load the titanic dataset
         titanic_data = sns.load_dataset('titanic')
         td = titanic_data.copy()
         td.drop(['alive', 'who', 'adult_male', 'class', 'embark_town', 'deck'], axis=1, inplace=True)
@@ -20,16 +19,13 @@ class TitanicAPI(Resource):
         td['sex'] = td['sex'].apply(lambda x: 1 if x == 'male' else 0)
         td['alone'] = td['alone'].apply(lambda x: 1 if x else 0)
 
-        # Encode categorical variables
         self.enc = OneHotEncoder(handle_unknown='ignore')
         embarked_encoded = self.enc.fit_transform(td[['embarked']].values.reshape(-1, 1))
         self.encoded_cols = self.enc.get_feature_names_out(['embarked'])
 
-        # Add the encoded columns to the DataFrame and drop the original 'embarked' column
         td[self.encoded_cols] = embarked_encoded.toarray()
         td.drop(['embarked'], axis=1, inplace=True)
 
-        # Train a logistic regression model with increased max_iter
         self.logreg = LogisticRegression(max_iter=1000)
         X = td.drop('survived', axis=1)
         y = td['survived']
@@ -37,20 +33,16 @@ class TitanicAPI(Resource):
 
     def predict_survival(self, data):
         try:
-            passenger = pd.DataFrame([data])  # Wrap data in a list to ensure DataFrame creation
-            # Preprocess the passenger data
+            passenger = pd.DataFrame([data]) 
             passenger['sex'] = passenger['sex'].apply(lambda x: 1 if x == 'male' else 0)
             passenger['alone'] = passenger['alone'].apply(lambda x: 1 if x else 0)
 
-            # Encode 'embarked' feature using the pre-fitted encoder
             embarked_encoded = self.enc.transform(passenger[['embarked']].values.reshape(-1, 1))
             passenger[self.encoded_cols] = embarked_encoded.toarray()
             passenger.drop(['embarked', 'name'], axis=1, inplace=True)
 
-            # Predict the survival probability for the new passenger
             dead_proba, alive_proba = np.squeeze(self.logreg.predict_proba(passenger))
 
-            # Return the survival probability
             return {
                 'Death probability': '{:.2%}'.format(dead_proba),
                 'Survival probability': '{:.2%}'.format(alive_proba)
@@ -67,6 +59,5 @@ class TitanicAPI(Resource):
         except Exception as e:
             return jsonify({'error': str(e)})
 
-# Define API resources
 api.add_resource(TitanicAPI, '/predict')
 
